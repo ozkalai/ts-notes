69d72a1230aa3fba6028cb9450217f02d4f012a0
ozkalai <melihozkalayy@gmail.com>
2021-08-03 16:33:54 +0300

    array and object types

---
+++ 2-basic-types/core-types/app.ts
@@ -0,0 +1,24 @@
+// object types 
+
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: "Melih",
+    age: 25
+}
+
+// array types 
+
+const personWithAdditionalFeatures = {
+    name: "Melih",
+    age:25,
+    hobbies: ["Sports" , "Cooking"]
+}
+
+let favoriteHobbies: string[];
+
+favoriteHobbies = ["Sports"]
+
+
+
