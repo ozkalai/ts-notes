022b58e52894821bd8c511585b530c146d0425e2
ozkalai <melihozkalayy@gmail.com>
2021-08-04 10:24:24 +0300

    void

---
+++ 2-basic-types/2.7-function-return/app.ts
@@ -0,0 +1,13 @@
+function add(n1: number , n2: number ) {
+    return n1 + n2;
+}
+
+function printResult(num: number): void {
+    console.log("Result:" + num)
+}
+
+printResult(add(5 ,12)); // printResult has a special return type -> void
+// you can check it out hover on printResult function
+// The function execute successfully but return anything that what is void type
+// the difference between undefined type and void type is technically undefined type expect 
+// return even your function does not return anything , void not.
